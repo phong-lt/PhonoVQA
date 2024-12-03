5b10b11c818977fea56932876fea7b2773bb6f5d
Nguyễn Thái Nhật An <166610549+Beoconcuame@users.noreply.github.com>
2024-12-03 09:45:45 +0700

    Update phoneme_utils.py

--- core/model/modules/phoneme_utils.py
+++ core/model/modules/phoneme_utils.py
@@ -33,3 +33,38 @@ class PhonemeEmbedding(nn.Module):
         word_embeddings = torch.cat((onset_emb, rhyme_emb, tone_emb), dim=-1)
 
         return word_embeddings
+    
+class PhonemeEmbedding_v2(nn.Module):
+    def __init__(self, 
+                onset_vocab_size,
+                rhyme_vocab_size,
+                tone_vocab_size,
+                onset_embed_dim=256,
+                rhyme_tone_embed_dim=256,):
+        super(PhonemeEmbedding_v2, self).__init__()
+        
+
+        self.onset_embed_dim = onset_embed_dim
+        self.rhyme_tone_embed_dim = rhyme_tone_embed_dim
+
+        self.onset_embedding = nn.Embedding(onset_vocab_size, onset_embed_dim)
+        self.rhyme_embedding = nn.Embedding(rhyme_vocab_size, rhyme_tone_embed_dim)
+        self.tone_embedding = nn.Embedding(tone_vocab_size, rhyme_tone_embed_dim)
+      
+
+    def forward(self, phoneme_tensor):
+        onset_indices = phoneme_tensor[:, :, 0]
+        rhyme_indices = phoneme_tensor[:, :, 1]
+        tone_indices = phoneme_tensor[:, :, 2]
+
+        onset_mask = onset_indices == 1
+        rhyme_mask = rhyme_indices == 1
+        tone_mask = tone_indices == 1 
+
+        onset_emb = self.onset_embedding(onset_indices) * math.sqrt(self.onset_embed_dim)
+        rhyme_emb = self.rhyme_embedding(rhyme_indices) * math.sqrt(self.rhyme_tone_embed_dim)
+        tone_emb = self.tone_embedding(tone_indices) * math.sqrt(self.rhyme_tone_embed_dim)
+
+
+
+        return onset_emb, rhyme_emb, tone_emb, onset_mask, rhyme_mask, tone_mask
