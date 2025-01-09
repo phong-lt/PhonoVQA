a56c49b3ed1b0435b631e23131a026cc4cbef762
Nguyễn Thái Nhật An <166610549+Beoconcuame@users.noreply.github.com>
2025-01-10 02:00:50 +0700

    Update __init__.py

--- core/tokenizer/__init__.py
+++ core/tokenizer/__init__.py
@@ -2,3 +2,4 @@ from .bpe_tokenizer import BPE_Tokenizer
 from .byte_tokenizer import ByteTokenizer
 from .char_tokenizer import CharTokenizer
 from .phoneme_tokenizer import PhonemeTokenizer
+from .eng_tokenizer import CharTokenizerEn
